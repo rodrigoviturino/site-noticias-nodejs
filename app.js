21c99d8e91fa44f29ca770a7c2fb0f14a40dd2a1
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-28 01:14:05 -0300

    Iniciando com Consign em server.js
    Adicionando o consign com o INCLUDE de todas as ROUTES que estava no
    app.js, tirando as routes do app e colocando com o INCLUDE no serve.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const app = require('./config/server');
 
-let rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+// let rotaNoticias = require('./app/routes/noticias');
+// rotaNoticias(app);
 
-let rotaHome = require('./app/routes/home');
-rotaHome(app);
+// let rotaHome = require('./app/routes/home');
+// rotaHome(app);
 
-let rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
-rotaFormInclusaoNoticia(app);
+// let rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
+// rotaFormInclusaoNoticia(app);
 
 app.listen(3000, function(){
     console.log('Servidor ON');    
--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const consign = require('consign');
 
+const app = express();
 // Motor de Geração de VIEWS
 app.set('view engine', 'ejs'); 
 app.set('views', './app/views');
 
+
+// Ele vai scanear o DEFAULT e encontrar o ROUTES e jogar dentro do APP
+consign().include('app/routes').into(app);
 
 module.exports = app;
