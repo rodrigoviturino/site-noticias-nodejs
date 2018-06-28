1b3d7efc9fecc2df79686d1d15527a3f64c8039f
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-06-28 00:40:23 -0300

    Adicionando mysql com node
    Adicionando o banco de dados MYSQL para integrar com o node, para depois
    usar o mongodb

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,23 @@
 module.exports = function (app) {
+
     app.get('/noticias', function(req,res){
-        res.render("noticias/noticias");
+
+        // ESTRUTURA SQL
+            let mysql = require('mysql');
+            // Estrutura JSON
+            let connection = mysql.createConnection({
+                host : 'localhost',
+                user : 'root',
+                password : '1234',
+                database : 'portal_noticias'
+            });
+
+            //connection.query(<sql>, <func callback>);
+            // Só vamos enviar o resultado para quem fizer a requisição 
+            connection.query('select * from noticias', function(error, result){
+                            // VIEW                 // JSON
+                res.render("noticias/noticias", {noticias : result });
+            });
+        // FIM SQL
     });
 };    
